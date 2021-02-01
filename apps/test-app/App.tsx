b956022be65afd30844df041d814dfbde6257c26
Esenda Obey <eobey@mi9retail.com>
2021-02-01 22:05:11 +0100

    display text-input component

--- apps/test-app/App.tsx
+++ apps/test-app/App.tsx
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+// making sure exists in the application
+console.log('MaterialCommunityIcons', MaterialCommunityIcons);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text testID="heading">
+      <Text testID="heading" style={styles.marginBottom}>
         Welcome! Open up App.tsx to start working on your app!
       </Text>
+      <MaterialCommunityIcons style={styles.marginBottom} name="account-edit" size={24} color="black" />
+      <TextInput label="name" right={<TextInput.Icon name='close' />} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +23,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 40,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  marginBottom: {
+    marginBottom: 40,
+  }
 });
